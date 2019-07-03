ee171f8b40d09737db113475bd95f9e261f03ce4
christhepirate <52164377+christhepirate@users.noreply.github.com>
2019-07-03 09:17:43 -0700

    Add files via upload

--- listsExamples.py
+++ listsExamples.py
@@ -1,4 +1,5 @@
 #lists of things
+#lists values are seperated/delimited w/ comma
 spam = ['cat', 'bat', 'rat', 'eliphant',]
 
 #prints the entire contents of the list spam.
@@ -15,6 +16,7 @@ bacon = [spam, [10, 20, 30, 40]]
 
 #prints the entire contents of the list bacon, which inludes the list spam.
 print(bacon)
+print('That last one was bacon')
 
 #prints the first value in bacon which is the entire list spam
 print(bacon[0])
@@ -31,18 +33,51 @@ print('The ' + bacon[0] [-1] + ' is afraid of the ' + str(bacon[1] [2]) + ' ' + 
 #these are all single value indexes
 #a list of values is a slice
 #notice the seperation w/ a colon
-print(spam[0:2])
-print(bacon [0] [0:2])
+print(spam[0:3])
+
+#a slice goes up to, but will not include, the value at the second index
+
+print(bacon [0] [1:4])
+
+#short cut for 0 in the index can be blank
+print(bacon [0] [:4])
+print(bacon [0] [:3])
+
+
+print(bacon [1] [:])
+
 
 #make a list from contents of a list
 eggs = bacon[1]
-print(str(eggs))
+print(eggs)
+print('That was eggs...')
 
 #math from list values
 print(str(eggs[1]+eggs[2]))
 
+#len of a list displays how many items are within it
+print(str(len(spam)))
 
+print('------------------------------------------')
 
+print(spam)
+#lets change the values inside of the list
+spam[1] = 'aardvark'
+print(spam)
+
+#changes rat to aardvark
+spam[2] = spam[1]
+print(spam)
+
+#we can also use negative indexes
+spam[-1] = spam[2]
+print(spam)
+
+#lets get a little more complex
+#this will change the value at the start of the list 'cat'
+#cat becomes a list within a list that contains the slice of spam that contains 3 ardvarks
+spam[0] = spam[1:4]
+print(spam)
 
 
 
